fed66b9226d12e93d8f29ada8a841ed515b5d761
Mark Chimes <chimes.mark@gmail.com>
2020-07-11 13:21:16 +0200

    Lag testing working on all keypresses.

--- Player.gd
+++ Player.gd
@@ -10,10 +10,26 @@ var y_velo = 0
 var facing_right = false
 var coyote_time = 5
 var game_paused = false;
+var kinematic_body;
+var body_start_pos;
 
 var is_right = false
+var is_left = false
+var is_jump = false
 
-enum {RIGHT_PRESS, RIGHT_RELEASE}
+enum {RIGHT_PRESS, RIGHT_RELEASE, LEFT_PRESS, LEFT_RELEASE, JUMP_PRESS, JUMP_RELEASE}
+var press_dir = {
+	"right": RIGHT_PRESS,
+	"left": LEFT_PRESS,
+	"jump": JUMP_PRESS
+}
+
+var release_dir = {
+	"right": RIGHT_RELEASE,
+	"left": LEFT_RELEASE,
+	"jump": JUMP_RELEASE
+}
+
 var key_presses = []
 var key_timings = []
 var dLag = 0.0
@@ -23,7 +39,9 @@ var next_lag = 0.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	update_lag_and_label(0.2)
-	update_next_lag_and_label(0.2)
+	update_next_lag_and_label(0.2)	
+	kinematic_body = get_node("KinematicBody2D")
+	body_start_pos = kinematic_body.get_position()
 
 func _input(event):
 	if(event.is_action_pressed("pause")):
@@ -35,37 +53,28 @@ func _input(event):
 	if game_paused:
 		return
 
-	foo(event, "right", RIGHT_PRESS, RIGHT_RELEASE)
+	add_key_event_to_conveyor(event)
 	
 	# Test purposes
 	if event.is_action_pressed("lag_up"):
-		update_next_lag_and_label(next_lag + 0.2)
+		update_next_lag_and_label(next_lag + 0.05)
 
 	if event.is_action_pressed("lag_down"):
-		update_next_lag_and_label(next_lag - 0.2)
-
-func foo(event, key_name, key_press_enum, key_release_enum):
-	if event.is_action_pressed(key_name):
-		key_presses.append(key_press_enum)
-		key_timings.append(lag)
-
-	if event.is_action_released(key_name):
-		key_presses.append(key_release_enum)
-		key_timings.append(lag)
+		update_next_lag_and_label(next_lag - 0.05)
 
 func _physics_process(delta):
 	if game_paused:
 		return
 
-	lag_adjustment(delta)
-	rotate_keypress_conveyor(delta)
+	adjust_lag(delta)
+	rotate_key_event_conveyor(delta)
 
 	var move_dir = 0
 	if is_right:
 		move_dir += 1
-	if Input.is_action_pressed("left"):
+	if is_left:
 		move_dir -= 1
-	var kinematic_body = get_node("KinematicBody2D")
+	kinematic_body = get_node("KinematicBody2D")
 	kinematic_body.move_and_slide(Vector2(move_dir * MOVE_SPEED, y_velo), Vector2(0, -1))
 	
 	var grounded = kinematic_body.is_on_floor()
@@ -74,7 +83,7 @@ func _physics_process(delta):
 	y_velo += GRAVITY
 #	if coyote_time > 0:
 #		print("coyote_time")
-	if coyote_time > 0 and Input.is_action_just_pressed("jump"):
+	if coyote_time > 0 and is_jump:
 		y_velo = -JUMP_FORCE
 	if grounded and y_velo >= 5:
 		y_velo = 5
@@ -83,7 +92,7 @@ func _physics_process(delta):
 	coyote_time -= delta * 100
 #	print(delta)
 
-func lag_adjustment(delta): 
+func adjust_lag(delta): 
 	dLag = 0
 	if abs(lag - next_lag) > 0.000001:
 		#lag adjustment
@@ -97,7 +106,19 @@ func lag_adjustment(delta):
 	
 	print(dLag)
 
-func rotate_keypress_conveyor(delta):
+
+func add_key_event_to_conveyor(event):
+	for key_name in press_dir: 
+		if event.is_action_pressed(key_name):
+			key_presses.append(press_dir[key_name])
+			key_timings.append(lag)
+	
+	for key_name in release_dir: 
+		if event.is_action_released(key_name):
+			key_presses.append(release_dir[key_name])
+			key_timings.append(lag)
+
+func rotate_key_event_conveyor(delta):
 	for i in range(0, key_timings.size()): 
 		key_timings[i] = key_timings[i] - delta + dLag
 		print("key_timings[i]")
@@ -111,6 +132,14 @@ func rotate_keypress_conveyor(delta):
 				is_right = true
 			RIGHT_RELEASE:
 				is_right = false
+			LEFT_PRESS:
+				is_left = true
+			LEFT_RELEASE:
+				is_left = false
+			JUMP_PRESS:
+				is_jump = true
+			JUMP_RELEASE:
+				is_jump = false
 
 func update_lag_and_label(new_lag): 
 	if new_lag < 0: 
