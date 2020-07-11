79bea7bde1b4edac5909905a9acdf39e10773f26
Mark Chimes <chimes.mark@gmail.com>
2020-07-11 12:25:14 +0200

    Lag right key press.

--- Player.gd
+++ Player.gd
@@ -13,6 +13,11 @@ var game_paused = false;
 
 var is_right = false
 
+enum {RIGHT_PRESS, RIGHT_RELEASE}
+var key_presses = []
+var key_timings = []
+var lag = 0.2
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -24,16 +29,38 @@ func _input(event):
 		else:
 			game_paused = true
 
+
+	if game_paused:
+		return
+
 	if event.is_action_pressed("right"):
-		is_right = true
+		key_presses.append(RIGHT_PRESS)
+		key_timings.append(lag)
 
 	if event.is_action_released("right"):
-		is_right = false
-
+		key_presses.append(RIGHT_RELEASE)
+		key_timings.append(lag)
 
 func _physics_process(delta):
 	if game_paused:
 		return
+	
+	for i in range(0, key_timings.size()): 
+		key_timings[i] -= delta
+		print("key_timings[i]")
+		print(key_timings[i])
+
+	while (key_timings.size() > 0 and key_timings[0] <= 0):
+		key_timings.pop_front()
+		var press = key_presses.pop_front()
+		match (press):
+			RIGHT_PRESS:
+				is_right = true
+			RIGHT_RELEASE:
+				is_right = false
+	
+
+
 	var move_dir = 0
 	if is_right:
 		move_dir += 1
@@ -46,8 +73,8 @@ func _physics_process(delta):
 	if grounded:
 		coyote_time = 20
 	y_velo += GRAVITY
-	if coyote_time > 0:
-		print("coyote_time")
+#	if coyote_time > 0:
+#		print("coyote_time")
 	if coyote_time > 0 and Input.is_action_just_pressed("jump"):
 		y_velo = -JUMP_FORCE
 	if grounded and y_velo >= 5:
@@ -55,4 +82,4 @@ func _physics_process(delta):
 	if y_velo > MAX_FALL_SPEED:
 		y_velo = MAX_FALL_SPEED
 	coyote_time -= delta * 100
-	print(delta)
+#	print(delta)
